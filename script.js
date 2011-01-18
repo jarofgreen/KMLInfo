7e19db5042b31556c6dc164c0f837f9c26153476
James <james@jarofgreen.co.uk>
2011-01-18 21:15:28 +0000

    compress

--- script.js
+++ script.js
@@ -14,7 +14,8 @@ $(document).ready(function() {
 var totalDist = 0.0;
 
 function parseXml(xml) {
-	var lastLat, lastLng = null;
+	var lastPoint = null;
+	var point;
 
 	$(xml).find("Placemark").each(function(){
 		var bits = $(this).find("Point").find("coordinates").text().split(",");
@@ -22,16 +23,15 @@ function parseXml(xml) {
 		var thisLng = parseFloat(bits[1]);
 		$("#Debug").append(thisLat+" , "+thisLng + "<br>");
 
-		if (lastLat && lastLng && thisLat && thisLng) {
-			var p1 = new LatLon(thisLat, thisLng);
-			var p2 = new LatLon(lastLat, lastLng);
-			var dist = parseFloat(p1.distanceTo(p2));
+		point = new LatLon(thisLat, thisLng);
+
+		if (lastPoint) {			
+			var dist = parseFloat(lastPoint.distanceTo(point));
 			totalDist = totalDist + dist;
 			$("#Debug").append(dist + "<br>");
 		}
 
-		lastLat = thisLat;
-		lastLng = thisLng;
+		lastPoint = point;
 	});
 	$("#Distance").append(totalDist + "<br>");
 
