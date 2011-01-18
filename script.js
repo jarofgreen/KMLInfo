3b8b51f561c2a3e3b06d7b2f51b9447a9dff5495
James <james@jarofgreen.co.uk>
2011-01-18 20:52:28 +0000

    load data

--- script.js
+++ script.js
@@ -2,5 +2,23 @@
 
 $(document).ready(function() {
 
+	$.ajax({
+	    type: "GET",
+	    url: "data.kml",
+	    dataType: "xml",
+	    success: parseXml
+	  });
+
 });
+
+function parseXml(xml) {
+	$(xml).find("Placemark").each(function(){
+		var pm = $(this);
+		var p = pm.find("Point");
+		var c = p.find("coordinates");
+		var bits = c.text().split(",");
+		$("#Debug").append(bits[0]+" , "+bits[1] + "<br />");
+	});
+}
+
 
